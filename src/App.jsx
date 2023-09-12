cae6377495433314f1eff7d6653051d12b10b0a9
imtiazsakib15 <sakib.imtiaz15@gmail.com>
2023-09-13 02:24:35 +0600

    added Navbar component for navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <header className="container mx-auto px-4 md:px-10 lg:px-36">
+        <Navbar></Navbar>
+      </header>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import profile from "../../assets/images/profile.png";
+
+const Navbar = () => {
+  return (
+    <div className="flex justify-between items-center border-b-2 pt-8 pb-6">
+      <h2 className="text-3xl font-bold">Knowledge Cafe</h2>
+      <img src={profile} alt="profile" />
+    </div>
+  );
+};
+
+export default Navbar;
